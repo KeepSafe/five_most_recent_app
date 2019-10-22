0031f8ec2eed2768dc01f18d12de16937891afe4
Lukasz Jachym <lukasz.jachym@gmail.com>
2019-10-22 12:51:30 +0200

    chore: auto migration performed by https://github.com/zendesk/zendesk_app_migrator

---
+++ v2/src/javascripts/legacy_app.js
@@ -0,0 +1,81 @@
+import BaseApp from "base_app";
+import helpers from "helpers";
+import Base64 from "base64";
+window.helpers = helpers;
+window.Base64 = Base64;
+
+const App = (function() {
+  return {
+    events: {
+      "app.activated": "init",
+      "fullUserData.done": "handleUserResults",
+      "requiredProperties.ready": "getUserData"
+    },
+    requests: {
+      fullUserData: function(userID) {
+        return {
+          url: helpers.fmt(
+            "/api/v2/users/%@/tickets/requested.json?sort_order=desc",
+            userID
+          ),
+          dataType: "json"
+        };
+      }
+    },
+    init: function(data) {
+      if (!data.firstLoad) {
+        return;
+      }
+
+      _.defer(
+        function() {
+          this.trigger("requiredProperties.ready");
+        }.bind(this)
+      );
+    },
+    getUserData: function() {
+      this.ajax(
+        "fullUserData",
+        this.ticket()
+          .requester()
+          .id()
+      );
+    },
+    handleUserResults: function(data) {
+      var lastestFive = _.first(data.tickets, 5).sort(function(a, b) {
+        var aID = a.id;
+        var bID = b.id;
+        return aID === bID ? 0 : aID < bID ? 1 : -1;
+      });
+
+      this.switchTo("lastfive", {
+        lastestFiveArr: lastestFive
+      });
+    },
+    safeGetPath: function(propertyPath) {
+      return _.inject(
+        propertyPath.split("."),
+        function(context, segment) {
+          if (context == null) {
+            return context;
+          }
+
+          var obj = context[segment];
+
+          if (_.isFunction(obj)) {
+            obj = obj.call(context);
+          }
+
+          return obj;
+        },
+        this
+      );
+    },
+    validateRequiredProperty: function(propertyPath) {
+      var value = this.safeGetPath(propertyPath);
+      return value != null && value !== "" && value !== "no";
+    }
+  };
+})();
+
+export default BaseApp.extend(App);
